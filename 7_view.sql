097bc8791626874fe6f8306656478777cb788207
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-10-20 23:58:48 +0300

    feat: 7 Task Done

--- 7_view.sql
+++ 7_view.sql
@@ -63,12 +63,18 @@ ORDER BY average_mark DESC;
 -- после выполнения которого у всех заказчиков без адреса в это поле добавится вопросительный знак.
 
 
-CREATE VIEW Customers AS
-SELECT p.last_name, p.first_name, p.phone, p.address
+-- CREATE VIEW Customers AS
+-- SELECT p.last_name, p.first_name, p.phone, p.address
+-- FROM person p
+--          JOIN owner ON p.person_id = owner.person_id
+--          JOIN order1 o ON owner.owner_id = o.owner_id
+-- GROUP BY p.last_name, p.first_name, p.phone, p.address;
+
+CREATE OR REPLACE VIEW Customers AS
+SELECT DISTINCT p.last_name, p.first_name, p.phone, p.address
 FROM person p
          JOIN owner ON p.person_id = owner.person_id
-         JOIN order1 o ON owner.owner_id = o.owner_id
-GROUP BY p.last_name, p.first_name, p.phone, p.address
+         JOIN order1 o ON owner.owner_id = o.owner_id;
 
 -- | last\_name | first\_name | phone | address |
 -- | :--- | :--- | :--- | :--- |
@@ -80,11 +86,11 @@ GROUP BY p.last_name, p.first_name, p.phone, p.address
 
 -- Так как все заказчики у нас с адресами, уберём это поле из нескольких, чтобы проверить
 -- Работоспособность оператора.
--- Уберём поле адресса у Petrov Petia и Galkina Galia и создадим представление заново
+-- Уберём поле адреса у Petrov Petia и Galkina Galia и обновим представление
 
 UPDATE person
 SET address = ''
-WHERE last_name IN ('Petrov', 'Galkina')
+WHERE last_name IN ('Petrov', 'Galkina');
 
 -- | last\_name | first\_name | phone | address |
 -- | :--- | :--- | :--- | :--- |
@@ -96,11 +102,42 @@ WHERE last_name IN ('Petrov', 'Galkina')
 
 
 -- Теперь уже напишем оператор, позволяющий добавить вопросительный знак в пустое поле адресса представления
+-- К сожалению, в postgresql нельзя писать update на представления, в которых есть логика group by или distinct
+-- Мы можем написать триггер, который будет срабатывать в случае обновления строк в view Customers
+
+CREATE OR REPLACE FUNCTION update_customers()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.address = '' THEN
+        NEW.address = '?';
+    END IF;
+
+    -- Update the row in the actual table
+    UPDATE person
+    SET address = NEW.address
+    WHERE last_name = NEW.last_name AND first_name = NEW.first_name;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER instead_of_update_customers
+    INSTEAD OF UPDATE ON Customers
+    FOR EACH ROW
+EXECUTE FUNCTION update_customers();
+
+
+-- Пример SQL-запроса обновления строки в представлении "Customers"
+-- При изменении адресса у Petrov Petia триггер будет срабатывать и ставить ? вместо пустой строки
 
 UPDATE Customers
-SET address = '?'
-FROM (SELECT p.address
-      FROM person p
-               JOIN owner o ON p.person_id = o.person_id
-               JOIN order1 ON o.owner_id = order1.owner_id) as sq
-WHERE sq.address <> ''
+SET address = ''
+WHERE last_name = 'Petrov' AND first_name = 'Petia';
+
+-- | last\_name | first\_name | phone | address |
+-- | :--- | :--- | :--- | :--- |
+-- | Sokolov | S. | +7678901234 | Srednii pr VO, 27-1 |
+-- | Ivanov | Vano | +7789012345 | Malyi pr VO, 33-2 |
+-- | Vasiliev | Vasia | +7345678901 | Nevskii pr, 9-11 |
+-- | Galkina | Galia | +7567890123 |  |
+-- | Petrov | Petia | +79234567890 | ? |
